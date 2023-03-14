8ebfa866db4a8becf3c4ed0d715f9922b023567b
Satish.Dungarani <satish2@amnex.com>
2023-03-14 08:45:17 +0000

    #Admin panel setup

--- Web/ImageProcessing.Web/Program.cs
+++ Web/ImageProcessing.Web/Program.cs
@@ -24,4 +24,16 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+   name: "areas",
+      pattern: "{area:}/{controller=Home}/{action=Index}/{id?}");
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllerRoute(
+//      name: "areas",
+//      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+//    );
+//});
+
 app.Run();
