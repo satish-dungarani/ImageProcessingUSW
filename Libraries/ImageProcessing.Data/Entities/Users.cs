e91033059b14f601ee9922431bdd9c9e717c4e73
Satish.Dungarani <satish2@amnex.com>
2023-03-24 12:40:27 +0000

    #USer entity added

---
+++ Libraries/ImageProcessing.Data/Entities/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing.Data
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DOB { get; set; }
+        public string Gender { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public int City { get; set; }
+        public int County { get; set; }
+        public string Postcode { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+    }
+}
